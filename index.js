f5a93e7e5769a31a6291e89287fa34c1b2602eb9
Yosua <yosua.halim@gmail.com>
2024-09-18 23:36:50 +0700

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const TelegramBot = require("node-telegram-bot-api");
 
 const { TOKEN, SERVER_URL } = process.env;
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
@@ -11,35 +12,61 @@ const WEBHOOK_URL = `${SERVER_URL}${URI}`;
 const app = express();
 app.use(bodyParser.json());
 
-const init = async () => {
-  try {
-    await axios.get(`${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`);
-    console.log("Webhook was set successfully");
-  } catch (error) {
-    console.error("Error setting webhook: ", error.message);
-  }
-};
+const bot = new TelegramBot(TOKEN);
+bot.setWebHook(WEBHOOK_URL);
+
+// bot.on("message", (message) => {
+//   console.log("Request received: ", message);
+
+//   const chatId = message.from.id;
+//   const text = message.text;
+
+//   switch (text) {
+//     case "/start":
+//       bot.sendMessage(
+//         chatId,
+//         "Hello, welcome Yosua and Ellena Dokimos Ark 🏠!"
+//       );
+
+//       break;
+//     case "/instagram":
+//       bot.sendMessage(
+//         chatId,
+//         "Feel free to follow us on Instagram: dokimos.ark https://www.instagram.com/dokimos.ark?igsh=ajZiODkyZHJpYWlo"
+//       );
+
+//       break;
+//     default:
+//       break;
+//   }
+// });
 
 app.post(URI, async (req, res) => {
   console.log("Request received: ", req.body);
 
-  const chatId = req.body.message.chat.id;
+  bot.processUpdate(req.body);
+
+  const chatId = req.body.message.from.id;
   const message = req.body.message.text;
 
   switch (message) {
     case "/start":
-      await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        chat_id: chatId,
-        text: "Hello, welcome Yosua and Ellena Dokimos Ark 🏠!",
-      });
+      bot.sendMessage(
+        chatId,
+        "Hello, welcome Yosua and Ellena Dokimos Ark 🏠!"
+      );
       break;
     case "/instagram":
-      await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        chat_id: chatId,
-        text: "Feel free to follow us on Instagram: dokimos.ark",
-      });
+      bot.sendMessage(
+        chatId,
+        "Feel free to follow us on Instagram: dokimos.ark https://www.instagram.com/dokimos.ark?igsh=ajZiODkyZHJpYWlo"
+      );
       break;
     default:
+      bot.sendMessage(
+        chatId,
+        `Here is our command: \n/start: To start \n/instagram: To see our instagram profile`
+      );
       break;
   }
 
@@ -48,5 +75,4 @@ app.post(URI, async (req, res) => {
 
 app.listen(5000, async () => {
   console.log("Server is running on port 5000");
-  await init();
 });
